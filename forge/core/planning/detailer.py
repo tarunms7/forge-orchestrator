51da1062c2edb60b8fd68acc55987dfa499d26a6
M Tarun <tarun@sourcewiz.co>
2026-03-17 23:39:08 +0530

    fix: detailer max turns increased

--- forge/core/planning/detailer.py
+++ forge/core/planning/detailer.py
@@ -43,7 +43,7 @@ class Detailer:
             prompt = self._build_prompt(task, sliced_map, conventions)
             options = ClaudeCodeOptions(
                 system_prompt=DETAILER_SYSTEM_PROMPT,
-                max_turns=3, model=self._model,
+                max_turns=5, model=self._model,
                 disallowed_tools=["Bash", "Glob", "Grep", "Task", "Edit", "Write"],
                 permission_mode="acceptEdits",
             )
